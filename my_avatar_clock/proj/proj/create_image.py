1b0f0331270fe7e1e7f7f4a493a853bef4266bf2
= <antsin93@gmail.com>
2020-05-14 22:38:11 +0300

    Optimise imports

--- my_avatar_clock/proj/proj/create_image.py
+++ my_avatar_clock/proj/proj/create_image.py
@@ -1,11 +1,18 @@
 import random
+from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
-from datetime import datetime
+
 from .settings import FONT_DIR
 
+CURRENT_FONT = f"{FONT_DIR}/orange juice 2.0.ttf"
 
-def create_image():
+
+def create_image() -> None:
+    """
+        Create image for telegram avatar.
+    """
+
     msg = datetime.now().strftime('%H:%M')
 
     W, H = (500, 500)
@@ -14,7 +21,7 @@ def create_image():
     img = Image.new('RGB', (W, H), color=(100, color_2, color_3))
 
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(f"{FONT_DIR}/orange juice 2.0.ttf", 240)
+    font = ImageFont.truetype(CURRENT_FONT, 240)
     w, h = draw.textsize(msg, font)
     draw.text(((W - w) / 2, (H - h) / 2), msg, fill=(255, 255, 0), font=font)
 
