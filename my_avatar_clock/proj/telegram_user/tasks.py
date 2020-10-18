6e67566842ea30b7cd3ecc871506ebb90cfba29d
ferom <antsin93@gmail.com>
2020-10-18 18:34:16 +0300

    Create tasks.py

---
+++ my_avatar_clock/proj/telegram_user/tasks.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+from datetime import datetime
+from .create_image import create_image
+from .telegram_avatar import change_photo, get_account
+from config.celery_app import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.task
+def telegram_change_avatar():
+    create_image()
+    account = get_account()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(change_photo(account))
+    logger.info(f'Avatar is changed in {datetime.now()}')
