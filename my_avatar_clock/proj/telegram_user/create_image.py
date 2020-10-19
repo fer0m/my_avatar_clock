ad5b543850e61486a6b32215179b06f6a58e27fc
ferom <antsin93@gmail.com>
2020-10-19 22:13:10 +0300

    Create new class AvatarCreator

--- my_avatar_clock/proj/telegram_user/create_image.py
+++ my_avatar_clock/proj/telegram_user/create_image.py
@@ -1,28 +1,58 @@
+import math
 import random
 from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
-
 from django.conf import settings
 
 CURRENT_FONT = f"{settings.FONT_DIR}/orange juice 2.0.ttf"
 
 
-def create_image() -> None:
-    """
-        Create image for telegram avatar.
-    """
+class AvatarCreator:
 
-    msg = datetime.now().strftime('%H:%M')
+    @classmethod
+    def create_image(cls) -> None:
+        """
+            Create image for telegram avatar.
+        """
+        width, height = 500, 500
 
-    W, H = (500, 500)
-    color_2 = random.randint(100, 200)
-    color_3 = random.randint(100, 200)
-    img = Image.new('RGB', (W, H), color=(100, color_2, color_3))
+        canvas = cls.create_canvas(width, height)
+        cls.draw_time_on_canvas(image=canvas)
 
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(CURRENT_FONT, 240)
-    w, h = draw.textsize(msg, font)
-    draw.text(((W - w) / 2, (H - h) / 2), msg, fill=(255, 255, 0), font=font)
+        canvas.save(settings.PICTURE_NAME)
 
-    img.save(settings.PICTURE_NAME)
+    @classmethod
+    def create_canvas(cls, width, height):
+        img_size = (width, height)
+
+        image = Image.new('RGB', img_size)
+
+        center_color = [random.randint(0, 150), random.randint(0, 150), random.randint(0, 150)]
+        corner_color = [random.randint(100, 240), random.randint(100, 240), random.randint(100, 240)]
+
+        for y in range(img_size[1]):
+            for x in range(img_size[0]):
+                distance_to_center = math.sqrt((x - img_size[0] / 2) ** 2 + (y - img_size[1] / 2) ** 2)
+                distance_to_center = float(distance_to_center) / (math.sqrt(2) * img_size[0] / 2)
+
+                rgb = []
+                for i in range(3):
+                    color = int(corner_color[i] * distance_to_center + center_color[i] * (1 - distance_to_center))
+                    rgb.append(color)
+                rgb.append(random.randint(0, 350))
+
+                image.putpixel((x, y), tuple(rgb))
+
+        return image
+
+    @classmethod
+    def draw_time_on_canvas(cls, image):
+        current_time = datetime.now().strftime('%H:%M')
+
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype(CURRENT_FONT, 240)
+
+        width_font, height_font = draw.textsize(current_time, font)
+        coordinate = ((image.width - width_font) / 2, (image.height - height_font) / 2)
+        draw.text(coordinate, current_time, fill=(255, 255, 255), font=font)
